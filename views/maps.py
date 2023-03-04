9c463c2d5d48686f2c7fbb66d36e8b07c5d46508
Ty Lovejoy <ty@fromskytocenter.com>
2023-03-04 08:22:22 -0600

    feat: visual overhaul of new maps embeds

--- views/maps.py
+++ views/maps.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
+from discord.utils import MISSING
 
 import utils
 import utils.utils
@@ -13,8 +15,76 @@ if TYPE_CHECKING:
     from core import DoomItx
 
 
+_MAPS_BASE_URL = "http://207.244.249.145/assets/images/map_banners/"
+
+
+@dataclass
+class MapMetadata:
+    NAME: str
+    COLOR: discord.Color
+    IMAGE_URL: str = ""
+
+    def __post_init__(self):
+        self.IMAGE_URL = _MAPS_BASE_URL + self._remove_extra_chars(self.NAME) + ".png"
+
+    def _remove_extra_chars(self, string: str):
+        return string.lower().replace(" ", "").replace(":", "").replace("'", "")
+
+
+all_map_constants = [
+    MapMetadata("Antarctic Peninsula", discord.Color.from_str("#29A0CC")),
+    MapMetadata("Ayutthaya", discord.Color.gold()),
+    MapMetadata("Black Forest", discord.Color.from_str("#94511C")),
+    MapMetadata("Blizzard World", discord.Color.from_str("#39AAFF")),
+    MapMetadata("Busan", discord.Color.from_str("#FF9F00")),
+    MapMetadata("Castillo", discord.Color.from_str("#E13C3C")),
+    MapMetadata("Chateau Guillard", discord.Color.from_str("#BCBCBC")),
+    MapMetadata("Circuit Royal", discord.Color.from_str("#00008B")),
+    MapMetadata("Colosseo", discord.Color.from_str("#BF7F00")),
+    MapMetadata("Dorado", discord.Color.from_str("#008a8a")),
+    MapMetadata("Ecopoint: Antarctica", discord.Color.from_str("#29A0CC")),
+    MapMetadata("Eichenwalde", discord.Color.from_str("#53E500")),
+    MapMetadata("Esperanca", discord.Color.from_str("#7BD751")),
+    MapMetadata("Hanamura", discord.Color.from_str("#EF72A3")),
+    MapMetadata("Havana", discord.Color.from_str("#00D45B")),
+    MapMetadata("Hollywood", discord.Color.from_str("#FFFFFF")),
+    MapMetadata("Horizon Lunar Colony ", discord.Color.from_str("#000000")),
+    MapMetadata("Ilios", discord.Color.from_str("#008FDF")),
+    MapMetadata("Junkertown", discord.Color.from_str("#EC9D00")),
+    MapMetadata("Kanezaka", discord.Color.from_str("#DF3A4F")),
+    MapMetadata("King's Row", discord.Color.from_str("#105687")),
+    MapMetadata("Lijiang Tower", discord.Color.from_str("#169900")),
+    MapMetadata("Malevento", discord.Color.from_str("#DDD816")),
+    MapMetadata("Midtown", discord.Color.from_str("#BCBCBC")),
+    MapMetadata("Necropolis", discord.Color.from_str("#409C00")),
+    MapMetadata("Nepal", discord.Color.from_str("#93C0C7")),
+    MapMetadata("New Queen Street", discord.Color.from_str("#CD1010")),
+    MapMetadata("Numbani", discord.Color.from_str("#3F921B")),
+    MapMetadata("Oasis", discord.Color.from_str("#C98600")),
+    MapMetadata("Paraiso", discord.Color.from_str("#19FF00")),
+    MapMetadata("Paris", discord.Color.from_str("#6260DA")),
+    MapMetadata("Petra", discord.Color.from_str("#DDD816")),
+    MapMetadata("Practice Range", discord.Color.from_str("#000000")),
+    MapMetadata("Rialto", discord.Color.from_str("#21E788")),
+    MapMetadata("Route 66", discord.Color.from_str("#FF9E2F")),
+    MapMetadata("Shambali", discord.Color.from_str("#2986CC")),
+    MapMetadata("Temple of Anubis", discord.Color.from_str("#D25E00")),
+    MapMetadata("Volskaya Industries", discord.Color.from_str("#8822DC")),
+    MapMetadata("Watchpoint: Gibraltar", discord.Color.from_str("#BCBCBC")),
+    MapMetadata("Workshop Chamber", discord.Color.from_str("#000000")),
+    MapMetadata("Workshop Expanse", discord.Color.from_str("#000000")),
+    MapMetadata("Workshop Green Screen", discord.Color.from_str("#3BB143")),
+    MapMetadata("Workshop Island", discord.Color.from_str("#000000")),
+    MapMetadata("Framework", discord.Color.from_str("#000000")),
+    MapMetadata("Tools", discord.Color.from_str("#000000")),
+]
+
+MAP_DATA: dict[str, MapMetadata] = {const.NAME: const for const in all_map_constants}
+
+
+
 class MapSubmit(discord.ui.Modal, title="MapSubmit"):
-    desc = discord.ui.TextInput(label="Description", style=discord.TextStyle.paragraph)
+    desc = discord.ui.TextInput(label="Description", style=discord.TextStyle.paragraph, required=False)
     levels = discord.ui.TextInput(
         label="Level Names",
         style=discord.TextStyle.paragraph,
@@ -52,17 +122,23 @@ class MapSubmit(discord.ui.Modal, title="MapSubmit"):
             map(str.strip, filter(lambda x: bool(x), self.levels.value.split("\n")))
         )
 
+        description = ""
+        if self.desc.value:
+            description = f"` Desc ` {self.desc}\n"
+
         embed = utils.DoomEmbed(
             title="Map Submission - Confirmation",
             description=(
-                f"┣ `Code` {self.data['map_code']}\n"
-                f"┣ `Map` {self.data['map_name']}\n"
-                f"┣ `Type` {', '.join(map_types)}\n"
-                f"┣ `Desc` {self.desc}\n"
-                f"┗ `Levels` {', '.join(levels)}\n"
+                f">>> ` Code ` {self.data['map_code']}\n"
+                f"`  Map ` {self.data['map_name']}\n"
+                f"` Type ` {', '.join(map_types)}\n" + description
             ),
+            color=MAP_DATA.get(self.data["map_name"], discord.Color.from_str("#000000")).COLOR,
+            image=MAP_DATA.get(self.data["map_name"], None).IMAGE_URL,
+            thumbnail=itx.client.user.display_avatar.url,
         )
-        embed = utils.set_embed_thumbnail_maps(self.data["map_name"], embed)
+        embed.add_field(name="Level Names (Each should be on separate lines)", value='\n'.join(levels))
+        # embed = utils.set_embed_thumbnail_maps(self.data["map_name"], embed)
         view = views.Confirm(itx, ephemeral=True)
         await itx.edit_original_response(
             content="Is this correct?", view=view, embed=embed
@@ -103,6 +179,7 @@ class MapSubmit(discord.ui.Modal, title="MapSubmit"):
         )
 
         embed.title = f"New Map by {self.data['creator_name']}"
+        embed.remove_field(0)
         new_map = await itx.guild.get_channel(utils.NEW_MAPS).send(embed=embed)
         await new_map.create_thread(name=f"Discuss {self.data['map_code']} here.")
         map_maker = itx.guild.get_role(746167804121841744)
