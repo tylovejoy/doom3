6da496cfe11638c72ba60b4546ff7a657671b86a
Ty <religiossdoge@gmail.com>
2024-05-05 17:54:21 -0500

    fix(starboard): starboard not working

--- core/events.py
+++ core/events.py
@@ -99,25 +99,118 @@ class BotEvents(commands.Cog):
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if payload.user_id == self.bot.user.id:
             return
-
         if payload.channel_id not in [utils.SPR_RECORDS, utils.RECORDS]:
             return
-
         if payload.emoji != discord.PartialEmoji.from_str(
             "<:upper:787788134620332063>"
         ):
             return
-
         query = "SELECT user_id, hidden_id FROM records WHERE message_id = $1;"
-        row: asyncpg.Record = await self.bot.database.fetchrow(query, payload.message_id)
+        row: asyncpg.Record = await self.bot.database.fetchrow(
+            query, payload.message_id
+        )
         if not row:
             return
         is_record = bool(row.get("user_id", None))
         is_record_queue = bool(row.get("hidden_id", None))
-
         if not (is_record or is_record_queue):
             return
+        vote_exists = await self._check_for_existing_vote(
+            payload.message_id, payload.channel_id, payload.user_id
+        )
+        if vote_exists:
+            return
+        query = """
+            SELECT 
+              COUNT(*) as count, 
+              max(top_record_id) as top_record_id
+            FROM top_records
+            WHERE original_message_id = $1
+              AND channel_id = $2
+            GROUP BY original_message_id, channel_id
+        """
+        top_record_data = await self.bot.database.fetchrow(
+            query, payload.message_id, payload.channel_id
+        )
+        if top_record_data is None:
+            top_record_id = None
+            count = 0
+        else:
+            top_record_id = top_record_data.get("top_record_id", None)
+            count = top_record_data.get("count", None)
 
+        async with self.bot.database.pool.acquire() as connection:
+            async with connection.transaction():
+                await self._insert_top_record_vote(
+                    payload.user_id,
+                    payload.message_id,
+                    payload.channel_id,
+                    top_record_id,
+                    connection=connection,
+                )
+                count += 1
+                if count < 10:
+                    return
+                content = f"{count} {self.upper_emoji_converter(count)} <#{payload.channel_id}>"
+                top_record_channel = self.bot.get_channel(utils.TOP_RECORDS)
+                if not top_record_id:
+                    await self._post_new_top_record(
+                        content, payload, top_record_channel, connection=connection
+                    )
+                else:
+                    await top_record_channel.get_partial_message(top_record_id).edit(
+                        content=content
+                    )
+
+    async def _post_new_top_record(
+        self, content, payload, top_record_channel, /, connection: asyncpg.Connection
+    ):
+        original_msg = await self.bot.get_channel(payload.channel_id).fetch_message(
+            payload.message_id
+        )
+        if not original_msg.embeds:
+            # return
+            ...
+        embed = original_msg.embeds[0]
+        embed.add_field(name="Original", value=f"[Jump!]({original_msg.jump_url})")
+        embed.colour = discord.Color.gold()
+        top_record_msg = await top_record_channel.send(content, embed=embed)
+        query = """UPDATE top_records SET top_record_id = $1 WHERE original_message_id = $2 AND channel_id = $3;"""
+        await self.bot.database.execute(
+            query,
+            top_record_msg.id,
+            payload.message_id,
+            payload.channel_id,
+            connection=connection,
+        )
+
+    async def _insert_top_record_vote(
+        self,
+        user_id: int,
+        message_id: int,
+        channel_id: int,
+        top_record_id: int,
+        /,
+        connection: asyncpg.Connection,
+    ):
+        query = """
+            INSERT INTO top_records (user_id, original_message_id, channel_id, top_record_id) 
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT (user_id, original_message_id, channel_id)
+            DO NOTHING;
+        """
+        await self.bot.database.execute(
+            query,
+            user_id,
+            message_id,
+            channel_id,
+            top_record_id,
+            connection=connection,
+        )
+
+    async def _check_for_existing_vote(
+        self, message_id: int, channel_id: int, user_id: int
+    ):
         query = """
             SELECT user_id
             FROM top_records
@@ -126,72 +219,8 @@ class BotEvents(commands.Cog):
               AND user_id = $3
             LIMIT 1;
         """
-
-        row = await self.bot.database.get_one(
-            query, payload.message_id, payload.channel_id, payload.user_id
-        )
-
-        if row:
-            return
-
-        query = """
-            SELECT COUNT(*) as count, top_record_id
-            FROM top_records
-            WHERE original_message_id = $1
-              AND channel_id = $2
-            GROUP BY original_message_id, channel_id, top_record_id
-        """
-
-        top_record_id = None
-        count = 0
-        async for row in self.bot.database.get(
-            query, payload.message_id, payload.channel_id
-        ):
-            count += row.count
-            if not top_record_id and row.top_record_id:
-                top_record_id = row.top_record_id
-
-        query = """
-            INSERT INTO top_records (user_id, original_message_id, channel_id, top_record_id) 
-            VALUES ($1, $2, $3, $4)
-            ON CONFLICT (user_id, original_message_id, channel_id)
-            DO NOTHING;
-        """
-
-        await self.bot.database.set(
-            query,
-            payload.user_id,
-            payload.message_id,
-            payload.channel_id,
-            top_record_id,
-        )
-
-        count += 1
-        if count < 10:
-            return
-
-        content = f"{count} {self.upper_emoji_converter(count)} <#{payload.channel_id}>"
-        top_record_channel = self.bot.get_channel(utils.TOP_RECORDS)
-
-        if not top_record_id:
-            original_msg = await self.bot.get_channel(payload.channel_id).fetch_message(
-                payload.message_id
-            )
-            if not original_msg.embeds:
-                return
-            embed = original_msg.embeds[0]
-            embed.add_field(name="Original", value=f"[Jump!]({original_msg.jump_url})")
-            embed.colour = discord.Color.gold()
-
-            top_record_msg = await top_record_channel.send(content, embed=embed)
-            query = """UPDATE top_records SET top_record_id = $1 WHERE original_message_id = $2 AND channel_id = $3;"""
-            await self.bot.database.set(
-                query, top_record_msg.id, payload.message_id, payload.channel_id
-            )
-        else:
-            await top_record_channel.get_partial_message(top_record_id).edit(
-                content=content
-            )
+        row = await self.bot.database.get_one(query, message_id, channel_id, user_id)
+        return row
 
     @staticmethod
     def upper_emoji_converter(stars: int) -> str:
